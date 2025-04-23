ad3800e9eb241c5ad00a8967867de26e465c413e
Mohammad Ismail Hossain <hmismail360@gmail.com>
2025-04-23 09:20:15 +0600

    array operation testing

--- unittesting/src/test/java/math/ArrayOperationsTesting.java
+++ unittesting/src/test/java/math/ArrayOperationsTesting.java
@@ -1,5 +1,29 @@
 package math;
 
-public class ArrayOperationsTesting {
+import io.FileIO;
+import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+
+public class ArrayOperationsTesting {
+    FileIO fileIO = new FileIO();
+    MyMath myMath = new MyMath();
+    ArrayOperations arrayOperations = new ArrayOperations();
+
+    @Test()
+    public void largeNumber() {
+        int[] actual = arrayOperations.findPrimesInFile(fileIO, "src/test/resources/highNumbers.txt", myMath);
+        int expected[] = {11, 13, 2, 103};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void smallNumber() {
+        int[] actual = arrayOperations.findPrimesInFile(fileIO, "src/test/resources/lowNumbers.txt", myMath);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void negativeNumber() {
+        int[] actual = arrayOperations.findPrimesInFile(fileIO, "src/test/resources/grades_valid.txt", myMath);
+    }
 }
